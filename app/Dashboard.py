0b0256bba28ce9b0ec0b170fe836a6f03be691c6
Fiambre <soy@fiambre.dev>
2025-04-06 09:18:52 -0400

    Refactor dashboard import and update rendering logic in Dashboard.py

--- app/Dashboard.py
+++ app/Dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import utils.set_logo as set_logo
-#import plugins.dashboard as dashboard
-
+import plugins.dashboard.dashboard as dashboard
 st.set_page_config(
     page_title="Xungungo Stocks",
     page_icon="👋",
@@ -10,6 +9,5 @@ st.set_page_config(
 
 set_logo.set_logo()
 
-#dashboard.get_dashboard()
-st.switch_page("pages/1_Stocks.py")
-
+dashboard.render()
+#st.switch_page("pages/1_Stocks.py")
